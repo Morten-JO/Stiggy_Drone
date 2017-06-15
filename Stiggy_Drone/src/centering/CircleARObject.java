ed7ca4d5e53a328225566e5f9594df60c3227e41
Tobias Frantsen <tobias-frantsen@hotmail.com>
2017-06-15 12:42:10 +0200

    Change if else state

--- Stiggy_Drone/src/centering/CircleARObject.java
+++ Stiggy_Drone/src/centering/CircleARObject.java
@@ -22,24 +22,9 @@ public class CircleARObject {
 		
 		System.out.println("BIAS:"+orientation);
 		
-		if (orientation < 88 && state == BasicController.SEARCHQR && oldTheta != orientation)
-		{
-			System.out.println("PaperChaseAutoController: Spin left");
-			drone.getCommandManager().spinLeft(20).doFor(200);
-			drone.getCommandManager().hover().doFor(500);
-			drone.getCommandManager().goRight(20).after(700);
-			drone.getCommandManager().hover().doFor(500);
-		}
-		else if (orientation > 91   && state == BasicController.SEARCHQR && oldTheta != orientation)
-		{
-			System.out.println("PaperChaseAutoController: Spin right");
-			drone.getCommandManager().spinRight(20).doFor(200);
-			drone.getCommandManager().hover().doFor(500);
-			drone.getCommandManager().goLeft(20).after(700);
-			drone.getCommandManager().hover().doFor(500);
-		}
+		
 			
-			else if(x > ((screenWidth/2)+xIntervals))
+		if(x > ((screenWidth/2)+xIntervals))
 			{
 			System.out.println("To center, go right.");
 			drone.getCommandManager().goRight(10).doFor(200);
@@ -65,13 +50,27 @@ public class CircleARObject {
 				return true;
 			}
 			System.out.println("To center, go up.");
-			drone.getCommandManager().down(10).doFor(120);
+			drone.getCommandManager().up(10).doFor(120);
 			drone.getCommandManager().hover().doFor(500);
-		}else{
+		}
+		else if (orientation < 88 && state == BasicController.SEARCHQR && oldTheta != orientation)
+		{
+			System.out.println("PaperChaseAutoController: Spin left");
+			drone.getCommandManager().spinLeft(20).doFor(200);
+			drone.getCommandManager().hover().doFor(500);
+			drone.getCommandManager().goRight(20).after(500);
+			drone.getCommandManager().hover().doFor(500);
+		}
+		else if (orientation > 91   && state == BasicController.SEARCHQR && oldTheta != orientation)
+		{
+			System.out.println("PaperChaseAutoController: Spin right");
+			drone.getCommandManager().spinRight(20).doFor(200);
+			drone.getCommandManager().hover().doFor(500);
+			drone.getCommandManager().goLeft(20).after(500);
+			drone.getCommandManager().hover().doFor(500);
+		}
+		else{
 			System.out.println("WE GOING FORWARD NOW BOIS!");
-			if (Values.DISTANCE > 200){
-				drone.getCommandManager().forward(20).doFor(300);
-			}
 			drone.getCommandManager().hover().doFor(1000);
 			return true;
 		}
